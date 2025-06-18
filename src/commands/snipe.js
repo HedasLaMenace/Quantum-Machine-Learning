7115ee3ea70f0d2e112442026777030be8e9adb1
HedasLaMenace <ffjniyen8@gmail.com>
2025-06-18 18:52:35 +0200

    Update snipe.js

--- src/commands/snipe.js
+++ src/commands/snipe.js
@@ -4,17 +4,22 @@ module.exports = {
   name: 'snipe',
   description: 'Affiche le dernier message supprimé dans ce salon',
   async execute(message) {
+    // Vérifie si la collection snipes existe sur le client
+    if (!message.client.snipes || !message.client.snipes.has(message.channel.id)) {
+      return message.channel.send("❌ Aucun message supprimé à snipper ici.").catch(() => {});
+    }
+
     const snipe = message.client.snipes.get(message.channel.id);
-    
+
     if (!snipe) {
       return message.channel.send("❌ Aucun message supprimé à snipper ici.").catch(() => {});
     }
-    
+
     const embed = new EmbedBuilder()
-      .setColor('#3498db') // Bleu agréable
+      .setColor('#3498db')
       .setDescription(snipe.content?.length > 0 ? snipe.content : '*[Message vide]*')
       .setAuthor({ name: snipe.author.tag, iconURL: snipe.author.displayAvatarURL?.() })
-      .setFooter({ text: `Message supprimé` })
+      .setFooter({ text: 'Message supprimé' })
       .setTimestamp(snipe.timestamp || Date.now());
 
     try {
